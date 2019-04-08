af8a825ed3c9afcf3851df18065d3339f68c1079
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2019-04-08 12:06:13 -0300

    media

--- Parte2/media.py
+++ Parte2/media.py
@@ -9,6 +9,34 @@
 #   - Vector ordenado de mayor a menor
 #
 
+def media_vector(data, arg, version):
+
+    media = 0
+    cantidad = 0
+    
+    for elemento in data:
+        media += elemento
+        cantidad += 1
+
+    media = media/cantidad
+
+    return ("Media-" + version, media)
+
+def media_lista(data, arg):
+
+    return media_vector(data, arg, "Lista")
+
+def media_vector_ord(data, arg):
+
+    return media_vector(data, arg, "Vector-Ordenado")
+
 def media(data, arg):
 
-    print("Media")
+    resultado = []
+
+    resultado.append(media_vector(data, arg, "Vector"))
+    resultado.append(media_lista(data, arg))
+    resultado.append(media_vector_ord(data, arg))
+
+    return resultado
+
