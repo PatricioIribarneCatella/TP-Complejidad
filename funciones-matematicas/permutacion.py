165f48cab516b292b7ee4f6a0c31f08583dd0bac
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2019-04-10 13:01:33 -0300

    Se modifica el algoritmo para el calculo de las permutaciones y  variaciones sin repetición

--- funciones-matematicas/permutacion.py
+++ funciones-matematicas/permutacion.py
@@ -10,34 +10,34 @@
 #   - Diccionario
 #
 
+def producto(*args, repeat=1):
+
+    pools = [tuple(pool) for pool in args] * repeat
+    result = [[]]
+    
+    for pool in pools:
+        result = [x+[y] for x in result for y in pool]
+    
+    for prod in result:
+        yield tuple(prod)
+
 def calcular_permutaciones(data, r=None):
-    
+
     pool = tuple(data)
-    
+
     n = len(pool)
+
     r = n if r is None else r
-    
-    if r > n:
-        return
-    
-    indices = list(range(n))
-    cycles = list(range(n, n-r, -1))
-    
-    yield tuple(pool[i] for i in indices[:r])
-    
-    while n:
-        for i in reversed(range(r)):
-            cycles[i] -= 1
-            if cycles[i] == 0:
-                indices[i:] = indices[i+1:] + indices[i:i+1]
-                cycles[i] = n - i
-            else:
-                j = cycles[i]
-                indices[i], indices[-j] = indices[-j], indices[i]
-                yield tuple(pool[i] for i in indices[:r])
-                break
-        else:
-            return
+
+    # Genera el producto cartesiano
+    # y filtra, luego de generar un set,
+    # las posibilidades que no tengan como
+    # longitud la especificada en 'r' (cantidad
+    # de elementos a tomar)
+    #
+    for indices in producto(range(n), repeat=r):
+        if len(set(indices)) == r:
+            yield tuple(pool[i] for i in indices)
 
 def permutaciones_vector(data, arg):
 
